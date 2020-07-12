24f0b1a0872b314e2679da7944fcad2c58397d40
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-07-12 20:59:07 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63217" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63256" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
